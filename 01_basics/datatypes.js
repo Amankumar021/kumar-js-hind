88069fa7b2a5e0aac853ea4bbc74f8b05794de63
Amankumar021 <aman706839@gmail.com>
2024-07-09 15:31:39 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS as newer version
+
+// alert(3 + 3)  // we arre using nodejs , not browser
+
+console.log(3 + 
+    3) // code readability should be high
+
+console.log("Aman");
+
+
+let name = "Aman"
+let age = 21
+let isLoggedIn = false
+let state ;
+
+
+// number => 2 to the power of 53
+// bigint (trading,)
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//object =>
+
+console.log(typeof "Aman");
+console.log(typeof age)
+console.log(typeof null)  // object
+console.log(typeof undefined) // undefined
