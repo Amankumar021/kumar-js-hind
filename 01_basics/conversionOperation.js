e8a0a0a993b892d3ea3a9201e617dfed2a0bbb55
Amankumar021 <aman706839@gmail.com>
2024-07-10 15:26:50 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,30 @@
+let score ="Aman"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 33
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Aman" => true
+
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
