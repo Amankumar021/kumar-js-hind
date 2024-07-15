febc3ccf774b7779f9b69c78444b7cfd5dc221f1
Aman_021 <aman706839@gmail.com>
2024-07-15 17:16:35 +0000

    numsMaths

---
+++ 01_basics/06_numsMaths.js
@@ -0,0 +1,26 @@
+const score = 400
+console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);   
+// console.log(balance.toFixed(2));      // to.Fixed : how many decimal digit u want     // output => 100.00
+// const otherNumber = 123.89666
+
+// console.log(otherNumber.toPrecision(3));      //    to.Precision : how precise value u want to display  // output =>  124
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));    //  to.LocalString : display numbers in comma, 'en-IN' use comma as Indian Standard.
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++   Maths  ++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));   // .abs => converts neg value into Positive Value.
+console.log(Math.round(4.6));  // .round => rounds off to nearest interger.
+console.log(Math.ceil(4.6));   // .ceil => takes top value // ex : (4.2) oe (4.9)  // Output => 5
+console.log(Math.floor(4.9));  // 
