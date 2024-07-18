4891b35d56fa4caeb248e47ba4a4e11bed8fdc32
Aman_021 <aman706839@gmail.com>
2024-07-18 15:37:25 +0000

    mathsAndnum

--- 01_basics/06_numsMaths.js
+++ 01_basics/06_numsMaths.js
@@ -19,8 +19,21 @@ const hundreds = 1000000
 
 // +++++++++++++++++++++++++++++++++++++   Maths  ++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));   // .abs => converts neg value into Positive Value.
-console.log(Math.round(4.6));  // .round => rounds off to nearest interger.
-console.log(Math.ceil(4.6));   // .ceil => takes top value // ex : (4.2) oe (4.9)  // Output => 5
-console.log(Math.floor(4.9));  // 
+// console.log(Math);
+// console.log(Math.abs(-4));   // .abs => converts neg value into Positive Value.
+// console.log(Math.round(4.6));  // .round => rounds off to nearest interger.
+// console.log(Math.ceil(4.6));   // .ceil => takes top value // ex : (4.2) oe (4.9)  // Output => 5
+// console.log(Math.floor(4.9));  // .floor => takes base value for output 
+// console.log(Math.min(6,8,9,3));  // .min => dispaly min value
+// console.log(Math.max(6,8,9,3));  /// .max => display max value
+
+
+console.log(Math.random()) ;  // Always took decimal value from 0 - 10 
+console.log((Math.random()*10) + 1);  //  display output greater then or equal to 1
+console.log(Math.floor(Math.random()*10) + 1); 
+
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )  
