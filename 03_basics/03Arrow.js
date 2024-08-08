c9c1fbd272968f99405b9b1a7a6e72d72246f208
Aman_021 <aman706839@gmail.com>
2024-08-08 07:03:16 +0000

    arrow

--- 03_basics/03Arrow.js
+++ 03_basics/03Arrow.js
@@ -5,7 +5,57 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username}, welcome to website`);  // .this => refer to current context (that is within this '{}'
+        console.log(this);
+        
     }
 }
 
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username= "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username ="Aman"
+//     console.log(this.username);
+    
+// }
+
+// chai()   // method used for calling a function    // output=> undefined
+
+// const chai = function (){     // arrow function
+//     let username = "aman"
+//     console.log(this.username);
+    
+// }
+
+// chai()  //  output=> undefined
+
+
+const chai =  () =>{    // arrow function
+    let username = "aman"
+    console.log(this.username);
+}
+
+// chai()    // ouput => undefined
+
+// const addTwo = (num1,num2) => {   //basic arrow function
+//     return num1 + num2  // explicit return
+// }
+// console.log(addTwo(3,8));
+
+// const addTwo = (num1,num2) =>   num1 + num2     // implicit return => maine maan liya 
+
+// const addTwo = (num1,num2) =>   (num1 + num2)
+
+// const addTwo = (num1,num2) =>   {username : "aman"}  // undefined value (without perinthisis)
+
+
+const addTwo = (num1,num2) =>  ( {username : "aman"})
+
+console.log(addTwo(3,8));
+
+const myArray = [ 1,2,3,4,4,5]
+ myArray.forEach(function(){})
+ myArray.forEach(() => {})
