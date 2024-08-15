3df1ae55e9331a9570964817fe94fa9c25f5ff23
Aman_021 <aman706839@gmail.com>
2024-08-15 16:18:23 +0000

    Call

---
+++ 03_basics/04_executionCallStack.js
@@ -0,0 +1,10 @@
+// jaavscript execution context (EC) :
+
+// {}=> global EC (this)
+// functional EC 
+// Eval EC (global object(mangoos))
+ 
+// Run phase
+// {} => memeory cration phase
+// execution phase
+// 
