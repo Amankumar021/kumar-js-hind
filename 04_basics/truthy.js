2768a623a818aad77effdc5f308d60db67d54442
Aman_021 <aman706839@gmail.com>
2024-09-01 19:32:43 +0000

    truthy

---
+++ 04_basics/truthy.js
@@ -0,0 +1,59 @@
+// const userEmail =""
+
+// if (userEmail) {
+//     console.log("Got usrer Email");
+    
+// } else  {
+//     console.log("Dont't have user email");
+    
+// }
+
+// notes 
+
+// falsy value
+// false, 0, -0, BigInt (0n) , "", null, undefined, NaN
+
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+    
+    
+// }
+
+ 
+// const emptyObj = {}
+
+// if (Oject.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+    
+// }
+
+
+// Nullish coalescing Operator (??): null undefined
+
+
+let val1 ;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null  ?? 10 ?? 10
+
+
+
+
+
+console.log(val1);
+
+// terniary Operator 
+
+// condition ? true : false
+
+const iceTaPrice = 100
+iceTaPrice <= 80 ? console.log("less than 80"): console.log("more than 80");
+
+
+
