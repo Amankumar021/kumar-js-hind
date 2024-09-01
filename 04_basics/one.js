5a88e47ca2f2f1e95eeb759988338ec92e78e426
Aman_021 <aman706839@gmail.com>
2024-09-01 19:30:29 +0000

    one

---
+++ 04_basics/one.js
@@ -0,0 +1,77 @@
+// if
+// const isUserLoggedIn = true
+// const temperature = 41
+
+
+
+// if (temperature === 40) {
+//      console.log("less then 50");
+     
+// } else {
+//      console.log("temperature is greater than 50");
+
+// }
+
+
+
+// <, > , <=,  ==, !=, ===, !==
+
+
+
+// const score = 200
+
+// if (score > 100) {
+//      let power = "fly"
+//      console.log(`user power : ${power}`);
+// }
+
+
+// console.log(`user power : ${power}`);   // in this case power is n ot defined
+
+
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");   // immature code   // one liner code
+
+
+// nesting
+
+// if (balance < 500) {
+//      console.log("less than 500");
+
+// } else  if (balance < 750) { 
+//      console.log("less t5han 750");
+
+// }else if (balance < 900) {
+//      console.log("less than 900");
+     
+// } else {
+//      console.log("less than 1200");
+     
+// } 
+
+
+
+// const UserLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle =false
+// const loggedInFromEmail = true
+
+// if (UserLoggedIn && debitCard && 2==3  ) {
+//      console.log("allowed to buy course");
+     
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail  ) {
+//      console.log("user looged in");
+     
+// }
+
+// here '&&' means both condition to mached to print the value  ,but 
+// for or'||' either condition to be matched to print the value
+
+
+
+
+
