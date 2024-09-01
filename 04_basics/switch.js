8caf75be4890b28c76b25e276273e1aa5430d8ee
Aman_021 <aman706839@gmail.com>
2024-09-01 19:31:56 +0000

    switch

---
+++ 04_basics/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "jan"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        
+        break;
+    case "feb":
+        console.log("feb");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        break;
+    case "april":
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
